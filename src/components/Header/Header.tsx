4dfe7c2e82d0ea2c8b91bf8154b277d0cedd9a26
bmwolf102990 <106914829+bmwolf102990@users.noreply.github.com>
2024-03-03 07:15:57 -0700

    reordered the mobile menu items and adjusted the border width for larger screens

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -26,32 +26,32 @@ export const Header = () => {
                 {isMobileMenu === true ? 
                     <ul className="absolute h-auto w-screen bg-white text-black text-center border-t-2 border-black">
                         <li className="py-3 border hover:bg-zinc-500 hover:text-white">Home</li>
+                        <li className="py-3 border hover:bg-zinc-500 hover:text-white">Pokémon Center</li>
+                        <li className="py-3 border hover:bg-zinc-500 hover:text-white">Trainer Resources</li>
+                        <li className="py-3 border hover:bg-zinc-500 hover:text-white">Trainer Dex</li>
                         <li className="py-3 border hover:bg-zinc-500 hover:text-white">Conferences</li>
                         <li className="py-3 border hover:bg-zinc-500 hover:text-white">Battle Tower</li>
                         <li className="py-3 border hover:bg-zinc-500 hover:text-white">Safari Battle Zone</li>
-                        <li className="py-3 border hover:bg-zinc-500 hover:text-white">Team & Trainer Dex</li>
-                        <li className="py-3 border hover:bg-zinc-500 hover:text-white">Trainer Resources</li>
-                        <li className="py-3 border hover:bg-zinc-500 hover:text-white">Pokémon Center</li>
-                        <li className="py-3 border hover:bg-zinc-500 hover:text-white">Sponsor Us</li>
+                        <li className="py-3 border hover:bg-zinc-500 hover:text-white *:hover:fill-white"><BsHeartFill className="inline fill-red-600 hover:parent:fill-white" /> Sponsor Us</li>
                     </ul>
                 :null}
             </header>
             <header className=" max-[1000px]:hidden z-10 sticky top-0">
                 <nav className="grid grid-cols-9 place-items-stretch w-screen bg-white text-black min-[2000px]:text-xl max-[1295px]:min-[1001px]:text-xs">
                     <Image 
-                        className="w-14 ml-5 border-y-8 border-white"
+                        className="w-14 ml-5 border-y-4 border-white"
                         src={PokeBall}
                         quality={100}
                         alt=""
                     />
-                    <button className="w-1/10 border-y-8 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Home</button>
-                    <button className="w-1/10 border-y-8 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Pokémon Center</button>
-                    <button className="w-1/10 max-[1295px]:min-[1001px]:px-2 border-y-8 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Trainer Resources</button>
-                    <button className="w-1/10 border-y-8 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Trainer Dex</button>
-                    <button className="w-1/10 border-y-8 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Conferences</button>
-                    <button className="w-1/10 border-y-8 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Battle Tower</button>
-                    <button className="w-1/10 max-[1295px]:min-[1001px]:px-2 border-y-8 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Safari Battle Zone</button>
-                    <button className="w-1/10 border-y-8 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal *:hover:fill-white"><BsHeartFill className="inline fill-red-600 hover:parent:fill-white" /> Sponsor Us</button>
+                    <button className="w-1/10 border-y-4 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Home</button>
+                    <button className="w-1/10 border-y-4 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Pokémon Center</button>
+                    <button className="w-1/10 border-y-4 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal max-[1295px]:min-[1001px]:px-2">Trainer Resources</button>
+                    <button className="w-1/10 border-y-4 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Trainer Dex</button>
+                    <button className="w-1/10 border-y-4 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Conferences</button>
+                    <button className="w-1/10 border-y-4 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal">Battle Tower</button>
+                    <button className="w-1/10 border-y-4 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal max-[1295px]:min-[1001px]:px-2">Safari Battle Zone</button>
+                    <button className="w-1/10 border-y-4 border-white hover:border-t-zinc-500 hover:border-b-red-600 hover:bg-zinc-500 hover:text-white hover:font-normal *:hover:fill-white"><BsHeartFill className="inline fill-red-600 hover:parent:fill-white" /> Sponsor Us</button>
                 </nav>
             </header>
         </>
