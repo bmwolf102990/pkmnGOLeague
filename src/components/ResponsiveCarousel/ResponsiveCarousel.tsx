3920a30ecc8b90449655483ae5517df1d8f0dcc3
bmwolf102990 <106914829+bmwolf102990@users.noreply.github.com>
2024-03-22 10:09:01 -0700

    added new images to public and adjusted league news image to be responsive

--- src/components/ResponsiveCarousel/ResponsiveCarousel.tsx
+++ src/components/ResponsiveCarousel/ResponsiveCarousel.tsx
@@ -5,7 +5,7 @@ import { BiEnvelope } from "react-icons/bi";
 import Image from "next/image";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import PokeBall from "../../../public/images/pokeball.png";
+import Piplup from "../../../public/images/piplup.png";
 
 export const ResponsiveCarousel = () => {
     const items = [
@@ -45,9 +45,9 @@ export const ResponsiveCarousel = () => {
             className=""
           >
             {items.map((item, i) => (
-              <div key={i} className="px-10 py-10 bg-gradient-to-br from-red-300 from-10% via-50% to-red-700 to-90% rounded">
-                <div className="mx-20">
-                  <Image src={PokeBall} width={100} height={100} alt="slides" />
+              <div key={i} className="px-10 py-10 bg-gradient-to-br from-zinc-400 from-10% via-50% to-zinc-800 to-90% rounded">
+                <div className="mx-16 min-[370px]:mx-20 min-[400px]:mx-24 min-[500px]:mx-32 min-[550px]:mx-40 min-[600px]:mx-44 min-[650px]:mx-52 min-[700px]:mx-64  min-[1000px]:mx-96 min-[1400px]:mx-[550px] min-[2000px]:mx-[800px] min-[2500px]:mx-[1100px]">
+                  <Image src={Piplup} width={100} height={100} quality={100} alt="slides" />
                 </div>
                 <div className="my-3">
                   <h2 className="mb-3 text-center">{item.title}</h2>
