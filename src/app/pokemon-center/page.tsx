7f4a0036fd52b20e88477dd171a80e98ac623627
bmwolf102990 <106914829+bmwolf102990@users.noreply.github.com>
2024-03-22 07:02:06 -0700

    adjusting responsive sizing for registration form

--- src/app/pokemon-center/page.tsx
+++ src/app/pokemon-center/page.tsx
@@ -15,15 +15,6 @@ export default function PokemonCenter() {
         <>
             <Header />
             <div className="z-0 flex flex-col h-auto w-screen bg-black">
-                <section>
-                    <Image 
-                    className="h-1/4 w-screen"
-                    src={PokeCenter}
-                    quality={100}
-                    fill={false}
-                    alt=""
-                    />
-                </section>
                 <section className="flex flex-col w-screen text-justify text-sm xl:text-lg min-[2000px]:text-2xl font-light px-6 xl:px-12 min-[2000px]:px-24 min-[600px]:text-center py-4 xl:py-8 min-[2000px]:py-16">
                     <h3 className="font-bold text-lg xl:text-2xl min-[2000px]:text-4xl text-center">Welcome to the Pokémon Center</h3>
                     <Image
@@ -42,11 +33,17 @@ export default function PokemonCenter() {
                         </button>
                     </a>
                 </section>
+                <section>
+                    <Image 
+                    className="h-1/4 w-screen"
+                    src={PokeCenter}
+                    quality={100}
+                    fill={false}
+                    alt=""
+                    />
+                </section>
                 <section className="flex flex-col w-screen text-justify text-sm xl:text-lg min-[2000px]:text-2xl px-6 xl:px-12 min-[2000px]:px-24 min-[600px]:text-center py-4 xl:py-8 min-[2000px]:py-16">
                     <h3 className="font-bold text-lg xl:text-2xl min-[2000px]:text-4xl text-center pb-8">League News</h3>
-                    {/* <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius accusantium explicabo, expedita maxime laborum odio, vel cumque nam laboriosam consectetur debitis eos aut nemo quisquam ratione, illo rerum deleniti harum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, in distinctio mollitia blanditiis illum officiis nobis numquam culpa ad vero consequatur ducimus et quae beatae tenetur totam! Nesciunt, explicabo soluta.
-                    </p> */}
                     <ResponsiveCarousel />
                 </section>
                 <section id="registration-section" className="flex flex-col w-screen text-justify text-sm xl:text-lg min-[2000px]:text-2xl px-3 xl:px-6 min-[2000px]:px-12 min-[600px]:text-center py-4 xl:py-8 min-[2000px]:py-16">
@@ -54,7 +51,7 @@ export default function PokemonCenter() {
                     <span className="py-2 text-xs text-center font-thin">
                         please upload an image of your QR Battle Code, from the Pokémon GO app, for other trainers to scan
                     </span>
-                    <form className="grid grid-col-1 gap-2 py-8 justify-center border rounded text-center">
+                    <form className="grid grid-col-1 gap-2 max-[385px]:py-8 max-[385px]:px-4 justify-center border rounded text-center">
                         <input className="p-1 rounded-sm text-black" type="text" placeholder="First Name" required/>
                         <input className="p-1 rounded-sm text-black" type="text" placeholder="Last Name"required/>
                         <input className="p-1 rounded-sm text-black" type="text" placeholder="Trainer Name" required/>
